aad30593912682b2a41328a86cbd42cd1d308cc1
Dipesh <dipesh.mobileappdev@gmail.com>
2024-08-26 18:19:02 +0530

    - init commit

---
+++ SegmentOptionsSwiftUI/SegmentOptionsSwiftUI/SegmentOptionsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SegmentOptionsSwiftUIApp.swift
+//  SegmentOptionsSwiftUI
+//
+//  Created by Dipesh Makvana on 26/08/24.
+//
+
+import SwiftUI
+
+@main
+struct SegmentOptionsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
